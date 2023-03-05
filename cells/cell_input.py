21b0dadfa5c5071a81179750bba6527b34e8616d
marcinwasowicz <marcinkwasowicz@gmail.com>
2023-03-05 16:19:10 +0100

    Implement script to train SICK regressor

--- cells/cell_input.py
+++ cells/cell_input.py
@@ -1,14 +1,18 @@
 class RecursiveCellInput:
-    def __init__(self, graph, per_node_embeddings, per_node_outputs):
+    def __init__(self, graph, per_node_embeddings, per_node_outputs=None):
         self.graph = graph
         graph.ndata["RecursiveCellInput.x"] = per_node_embeddings
-        graph.ndata["RecursiveCellInput.y"] = per_node_outputs
+        if per_node_outputs is not None:
+            graph.ndata["RecursiveCellInput.y"] = per_node_outputs
 
     def get_embeddings(self):
         return self.graph.ndata["RecursiveCellInput.x"]
 
     def get_outputs(self):
-        return self.graph.ndata["RecursiveCellInput.y"]
+        try:
+            return self.graph.ndata["RecursiveCellInput.y"]
+        except KeyError:
+            raise Exception("Per node outputs has not been set for input.")
 
     def get_graph(self):
         return self.graph
