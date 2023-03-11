0bfc5dd213c21fd7eafcbd9198239514cc1ba9d0
marcinwasowicz <marcinkwasowicz@gmail.com>
2023-03-11 22:37:54 +0100

    Simplify input space -> hidden space mapping

--- cells/single_forget_gate/mgu.py
+++ cells/single_forget_gate/mgu.py
@@ -11,10 +11,10 @@ class SingleForgetGateTreeMGU(nn.Module):
         self._n_ary = n_ary
         self._h_size = h_size
 
-        self.W = nn.Linear(x_size, 2 * h_size)
+        self.W = nn.Linear(x_size, h_size)
 
-        self.U_f = nn.Linear(n_ary * h_size, h_size, bias=False)
-        self.U_h_candidate = nn.Linear(n_ary * h_size, h_size, bias=False)
+        self.U_f = nn.Linear(n_ary * h_size, h_size)
+        self.U_h_candidate = nn.Linear(n_ary * h_size, h_size)
 
     def _message_function(self, edges):
         return {"h": edges.src["h"]}
@@ -26,35 +26,22 @@ class SingleForgetGateTreeMGU(nn.Module):
             size=(nodes.mailbox["h"].size(0), h_padding_size * self._h_size)
         )
         h_cat = th.cat((h, h_padding), dim=1)
-        f = self.U_f(h_cat)
-        return {"f": f, "h_cat": h_cat, "h": th.sum(nodes.mailbox["h"], 1)}
-
-    def _update_function(self, nodes):
-        wx = nodes.data["wx"]
-        w_h_candidate_x, w_f_x = th.tensor_split(wx, [self._h_size], 1)
-        f = th.sigmoid(nodes.data["f"] + w_f_x)
-        h_candidate = th.tanh(
-            w_h_candidate_x
-            + self.U_h_candidate(f.repeat(1, self._n_ary) * nodes.data["h_cat"])
-        )
-        h = f * nodes.data["h"] + (th.ones(*f.size()) - f) * h_candidate
+        f = th.sigmoid(self.U_f(h_cat))
+        h_candidate = th.tanh(self.U_h_candidate(f.repeat(1, self._n_ary) * h_cat))
+        h = f * th.sum(nodes.mailbox["h"], 1) + (th.ones(*f.size()) - f) * h_candidate
         return {"h": h}
 
     def forward(self, input: RecursiveCellInput):
         x = input.get_embeddings()
-        n = input.get_graph().number_of_nodes()
         nodes_generator = dgl.topological_nodes_generator(input.get_graph())
 
-        input.get_graph().ndata["wx"] = self.W(x)
-        input.get_graph().ndata["f"] = th.zeros((n, self._h_size))
-        input.get_graph().ndata["h"] = th.zeros((n, self._h_size))
-        input.get_graph().ndata["h_cat"] = th.zeros((n, self._n_ary * self._h_size))
+        initial_state = th.tanh(self.W(x))
+        input.get_graph().ndata["h"] = initial_state
 
         input.get_graph().prop_nodes(
             nodes_generator=nodes_generator,
             message_func=self._message_function,
             reduce_func=self._reduce_function,
-            apply_node_func=self._update_function,
         )
 
         return input.get_graph().ndata["h"]
