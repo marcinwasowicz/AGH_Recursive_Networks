43e282282d68acd48c4e1d8f9f68a83868809313
marcinwasowicz <marcinkwasowicz@gmail.com>
2023-04-10 09:40:15 +0200

    Optuna Integration POC

--- ares_template_gpu.sh
+++ ares_template_gpu.sh
@@ -30,6 +30,7 @@ pip install torch==1.11.0
 pip install scikit-learn==1.2.0
 pip install dgl -f https://data.dgl.ai/wheels/cu116/repo.html
 pip install dglgo -f https://data.dgl.ai/wheels-test/repo.html
+pip install optuna==3.1.1
 
 ##python e2e/sst_dataset/prepare_constituency_sst_embeddings.py config/sst_classification.json
 ##python e2e/sick_dataset/prepare_constituency_sick_embeddings.py config/sick_regression.json
